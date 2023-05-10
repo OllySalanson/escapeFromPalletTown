132c8ab6afba7869ab7e5bdd2388fa57ba98272a
Oliver Salanson <ollysalanson@gmail.com>
2023-05-10 14:46:07 +0100

    reduced collision size and altered start location

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -64,7 +64,7 @@ public class PlayerController : MonoBehaviour
 
     private bool IsWalkable(Vector3 targetPos)
     {
-        if(Physics2D.OverlapCircle(targetPos, 0.3f, solidObjectsLayer) != null)
+        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null)
         {
             return false;
         }
