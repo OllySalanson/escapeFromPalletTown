1baf3a95a2b1724bad1c9cb6028795df70ff64a9
Oliver Salanson <ollysalanson@gmail.com>
2023-05-10 21:11:55 +0100

    added moves and movebase

--- Assets/Scripts/Pokemons/Pokemon.cs
+++ Assets/Scripts/Pokemons/Pokemon.cs
@@ -2,17 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Pokemon : MonoBehaviour
+public class Pokemon : PokemonBase
 {
-    // Start is called before the first frame update
-    void Start()
+    PokemonBase _base;
+    int level;
+
+    public int HP {get; set;}
+
+    public List<Move> Moves {get; set;}
+
+    public Pokemon(PokemonBase pBase, int pLevel)
     {
-        
+        _base = pBase;
+        level = pLevel;
+        HP = _base.MaxHp;
+
+        //Generate Moves
+        Moves = new List<Move>();
+        foreach(var move in _base.LearnableMoves)
+        {
+            if(move.Level <= level)
+            {
+                Moves.Add(new Move(move.Base));
+            }
+
+            if(Moves.Count >= 4){
+                break;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public int Attack{
+        //this is the actual formula used in Pokemon
+        get {return Mathf.FloorToInt((_base.Attack * level) / 100f) + 5;}
+    }
+    public int Defense
     {
-        
+        get { return Mathf.FloorToInt((_base.Defense * level) / 100f) + 5; }
+    }
+
+    public int SpAttack
+    {
+        get { return Mathf.FloorToInt((_base.SpAttack * level) / 100f) + 5; }
+    }
+
+    public int SpDefense
+    {
+        get { return Mathf.FloorToInt((_base.SpDefense * level) / 100f) + 5; }
+    }
+
+    public int Speed
+    {
+        get { return Mathf.FloorToInt((_base.Speed * level) / 100f) + 5; }
+    }
+
+    // MaxHp adds 10 at the end instead of 5
+    public int MaxHp
+    {
+        get { return Mathf.FloorToInt((_base.MaxHp * level) / 100f) + 10; }
     }
 }
--- Assets/Scripts/Pokemons/PokemonBase.cs
+++ Assets/Scripts/Pokemons/PokemonBase.cs
@@ -23,6 +23,105 @@ public class PokemonBase : ScriptableObject
     [SerializeField] int spAttack;
     [SerializeField] int spDefense;
     [SerializeField] int speed;
+
+    [SerializeField] List<LearnableMove> learnableMoves;
+
+    // Properties below this line (C#'s answer to encapsulations e.g. getters and setters in Java)
+    // Name property
+    public string Name
+    {
+        get { return name; }
+    }
+
+    // Description property
+    public string Description
+    {
+        get { return description; }
+    }
+
+    // FrontSprite property
+    public Sprite FrontSprite
+    {
+        get { return frontSprite; }
+    }
+
+    // BackSprite property
+    public Sprite BackSprite
+    {
+        get { return backSprite; }
+    }
+
+    // Type1 property
+    public PokemonType Type1
+    {
+        get { return type1; }
+    }
+
+    // Type2 property
+    public PokemonType Type2
+    {
+        get { return type2; }
+    }
+
+    // MaxHp property
+    public int MaxHp
+    {
+        get { return maxHp; }
+    }
+
+    // Attack property
+    public int Attack
+    {
+        get { return attack; }
+    }
+
+    // Defense property
+    public int Defense
+    {
+        get { return defense; }
+    }
+
+    // SpAttack property
+    public int SpAttack
+    {
+        get { return spAttack; }
+    }
+
+    // SpDefense property
+    public int SpDefense
+    {
+        get { return spDefense; }
+    }
+
+    // Speed property
+    public int Speed
+    {
+        get { return speed; }
+    }
+
+    public List<LearnableMove> LearnableMoves
+    {
+        get {return learnableMoves;}
+    }
+        
+
+}
+
+[System.Serializable]
+public class LearnableMove
+{
+    [SerializeField] MoveBase moveBase;
+    [SerializeField] int level;
+
+    public MoveBase Base
+    {
+        get { return moveBase; }
+    }
+
+    public int Level
+    {
+        get { return level; }
+    }
 }
 
 public enum PokemonType
@@ -44,3 +143,6 @@ public enum PokemonType
     Ghost,
     Dragon
 }
+
+
+
