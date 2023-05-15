3b995abc627421e583d087b73d88dcffb9f0287e
Oliver Salanson <ollysalanson@gmail.com>
2023-05-15 15:53:34 +0100

    added action selectors

--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BattleState {Start, PlayerAction, PlayerMove, EnemyMove, Busy}
+
 public class BattleSystem : MonoBehaviour
 {
     [SerializeField] BattleUnit playerUnit;
@@ -10,18 +12,58 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] BattleHud enemyHud;
     [SerializeField] BattleDialogBox dialogBox;
 
+    BattleState state;
+    //if currentAction = 0 then fight, if currentAction 1 then run
+    int currentAction;
+
     private void Start()
     {
-        SetupBattle();
+        StartCoroutine(SetupBattle());
     }
 
-    public void SetupBattle() 
+    public IEnumerator SetupBattle() 
     {
         playerUnit.Setup();
         enemyUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
         enemyHud.SetData(enemyUnit.Pokemon);
 
-        dialogBox.SetDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared");
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
     }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    private void Update()
+    {
+        if(state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+    }
+
+    void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(currentAction < 1)
+                ++currentAction;
+        } 
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if(currentAction > 0)
+                --currentAction;
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+    }
+
+
 }
