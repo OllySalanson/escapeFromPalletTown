aa0a4526d0affb0f4b060070825e350c178ce4f0
Oliver Salanson <ollysalanson@gmail.com>
2023-05-22 17:10:42 +0100

    added sleep and frozen status effects

--- Assets/Scripts/Data/ConditionsDB.cs
+++ Assets/Scripts/Data/ConditionsDB.cs
@@ -35,10 +35,73 @@ public class ConditionsDB
                     pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name} hurt itself due to burn");
                 }
             }
+        },
+        {
+            ConditionID.par,
+            new Condition()
+            {
+                Name = "Paralyzed",
+                StartMessage = "has been paralyzed",
+                OnBeforeMove = (Pokemon pokemon) =>
+                {
+                    //1 in 4 times, a paralyzed pokemon will not be able to perform a move
+                    if(Random.Range(1, 5) == 1)
+                    {
+                        pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name}'s paralyzed and can't move");
+                        return false;
+                    }                       
+
+                    return true;
+                }
+            }
+        },
+        {
+            ConditionID.frz,
+            new Condition()
+            {
+                Name = "Freeze",
+                StartMessage = "has been frozen",
+                OnBeforeMove = (Pokemon pokemon) =>
+                {
+                    //1 in 4 times, a paralyzed pokemon will not be able to perform a move
+                    if(Random.Range(1, 5) == 1)
+                    {
+                        pokemon.CureStatus();
+                        pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name}'s not frozen any more");
+                        return true;
+                    }
+
+                    return false;
+                }
+            }
+        },
+        {
+            ConditionID.slp,
+            new Condition()
+            {
+                Name = "Sleep",
+                StartMessage = "has fallen asleep",
+                OnStart = (Pokemon pokemon) =>
+                {
+                    //Sleep for 1-3 turns
+                    pokemon.StatusTime = Random.Range(1,4);
+                    Debug.Log($"Will be asleep for {pokemon.StatusTime} moves");
+                },
+                OnBeforeMove = (Pokemon pokemon) =>
+                {
+                    if(pokemon.StatusTime <= 0)
+                    {
+                        pokemon.CureStatus();
+                        pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name} woke up!");
+                        return true;
+                    }
+                    pokemon.StatusTime--;
+                    pokemon.StatusChanges.Enqueue($"{pokemon.Base.Name} is sleeping");
+                    return false;
+                }
+            }
         }
     };
-
-
 }
 
 public enum ConditionID
